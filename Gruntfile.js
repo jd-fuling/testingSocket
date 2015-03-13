3da93352e514e270aaa5cecd0c86ff239d954ed8
jorgendrevland <jorgen.drevland@gmail.com>
2015-03-13 22:37:42 +0100

    Added Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+/**
+ * Created by jordrevl on 13.03.2015.
+ * Example file from gruntjs.com
+ */
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
