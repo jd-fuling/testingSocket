2599015f13e0f41151e85e0171ca3caa72ee97a7
jorgendrevland <jorgen.drevland@gmail.com>
2015-03-13 22:23:38 +0100

    Socket.io chat tutorial

--- index.js
+++ index.js
@@ -3,9 +3,16 @@
  */
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.send('<h1>working</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3005, function(){
